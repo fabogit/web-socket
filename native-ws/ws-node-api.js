bc538fcb721edf81cbf36bde00828c5e05eba211
fabo <fabo.dev.email@gmail.com>
2024-03-09 19:29:26 +0100

    #3
    format

--- native-ws/ws-node-api.js
+++ native-ws/ws-node-api.js
@@ -1,31 +1,30 @@
 import http from "http";
-import * as ws from 'ws';
-
+import * as ws from "ws";
 
 const httpServer = http.createServer((req, res) => {
-	res.end('Connected');
+	res.end("Connected");
 });
 
 // ws server will listen to http server
 const wsServer = new ws.WebSocketServer({
-	server: httpServer
+	server: httpServer,
 });
 
 // headers emitted before handshake to upgrade to ws protocol
-wsServer.on('headers', (headers, req) => {
+wsServer.on("headers", (headers, req) => {
 	console.log(headers);
 });
 
 // handshake is complete
-wsServer.on('connection', (websocket, req) => {
+wsServer.on("connection", (websocket, req) => {
 	// console.log(websocket)
-	websocket.send('Websocket server');
+	websocket.send("Websocket server");
 	// listen to client buffer
-	websocket.on('message', (data) => {
+	websocket.on("message", (data) => {
 		console.log(data.toString());
 	});
 });
 
 httpServer.listen(8000, () => {
-	console.log('Http server is up');
+	console.log("Http server is up");
 });
