47eb33e6f8fedb14cf807f844539267639dc7864
fabo <fabo.dev.email@gmail.com>
2024-03-10 22:25:13 +0100

    #15
    init

---
+++ 04/name-spaces/namespaces.js
@@ -0,0 +1,26 @@
+import path from "path";
+import express from "express";
+import { Server } from "socket.io";
+
+const app = express();
+
+// absolute path to the 'public' directory
+const publicPath = path.join(new URL(".", import.meta.url).pathname, "public");
+// Serve static files from the 'public' directory
+app.use(express.static(publicPath));
+
+const expressServer = app.listen(8000);
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+	console.log(socket.id, "Server connected");
+	// will emit event from the server
+	// socket.emit("messageFromServer", { data: `Socket server payload at ${now}` });
+
+	// listener will catch incoming event from the client
+	socket.on("newMessageToServer", (dataFromClient) => {
+		console.log("Data:", dataFromClient);
+		// emit event to all connected sockets
+		io.emit("newMessageToClients", { text: dataFromClient.text });
+	});
+});
